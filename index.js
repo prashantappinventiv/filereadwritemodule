eea33a3dde9980ae35d1530c25a1e069f885254d
prashant <prashant.sharma@appinventiv.com>
2023-07-05 06:20:58 +0530

    crud

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs=require("fs");
+//fs.mkdirSync("prasant");
+//fs.writeFileSync("prasant/intro.txt","my name is prashant");
+//fs.appendFileSync("prasant/intro.txt","at last perform append");
+//const data=fs.readFileSync("prasant/intro.txt","utf-8");
+//console.log(data);
+//rename
+//fs.renameSync("prasant/intro.txt","nodejs.txt");
+//fs.unlinkSync("nodejs.txt")
+fs.rmdirSync("prasant");
